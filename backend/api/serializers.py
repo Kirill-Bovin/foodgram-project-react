f3f44560c04661cb1e5b89de007dd804c4b4c0e5
Kirill Bovin <skrill96@e1.ru>
2023-08-18 22:04:30 +0500

    Chek

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -77,8 +77,9 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     author = UsersSerializer(read_only=True)
     tags = TagSerializer(many=True, read_only=True)
-    ingredients = RecipeIngredientSerializer(many=True, required=True,
-                                             source='recipe')
+    ingredients = RecipeIngredientSerializer(
+                                            many=True, required=True,
+                                            source='recipe')
     image = Base64ImageField()
     is_favorited = serializers.SerializerMethodField(read_only=True)
     is_in_shopping_cart = serializers.SerializerMethodField(read_only=True)
